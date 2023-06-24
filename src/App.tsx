bbe2d4ccb4fa56d30d43ac550242402d0c962b63
yooveloper <yooveloper@gmail.com>
2023-06-24 23:35:04 +0900

    build: add recoil and sample

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,7 @@ import NotFound from "pages/NotFound";
 import Intro from "pages/Intro";
 import Gift from "pages/Gift";
 import GlobalStyle from "style/GlobalStyle";
+import RecoilSample from "pages/RecoilSample";
 
 const SayHello = styled.h1`
   background: linear-gradient(to right, #f12711, #f5af19);
@@ -23,6 +24,7 @@ function App() {
           <Route path="*" element={<NotFound />} />
           <Route path="/" element={<Intro />} />
           <Route path="/gift" element={<Gift />} />
+          <Route path="/recoil-sample" element={<RecoilSample />} />
         </Routes>
       </header>
     </div>
--- src/index.tsx
+++ src/index.tsx
@@ -3,14 +3,23 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import { BrowserRouter as Router } from "react-router-dom";
+import {
+  RecoilRoot,
+  // atom,
+  // selector,
+  // useRecoilState,
+  // useRecoilValue,
+} from "recoil";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement,
 );
 root.render(
   <React.StrictMode>
-    <Router>
-      <App />
-    </Router>
+    <RecoilRoot>
+      <Router>
+        <App />
+      </Router>
+    </RecoilRoot>
   </React.StrictMode>,
 );
