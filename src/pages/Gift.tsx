9dfb8dca84d9e26cc3c7d8b284708c5fb84c7057
zwonkim <k980912@naver.com>
2023-06-24 22:55:35 +0900

    feat: 선물 추가 기능 구현

--- src/pages/Gift.tsx
+++ src/pages/Gift.tsx
@@ -1,5 +1,54 @@
-import React from "react";
+import React, { useState } from "react";
+
+interface LinkPreviewResponse {
+  author: string | null;
+  date: string | null;
+  title: string | null;
+  description: string | null;
+  image: string | null;
+  logo: string | null;
+  publisher: string | null;
+  url: string | null;
+}
 
 export default function Gift() {
-  return <h1>Gift Page</h1>;
+  const [url, setUrl] = useState<string>("");
+  const [response, setResponse] = useState<LinkPreviewResponse | null>(null);
+
+  const handleUrlChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setUrl(event.target.value);
+  };
+
+  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+
+    const data = {
+      key: process.env.REACT_APP_LINKPREVIEW_API_KEY,
+      q: url,
+    };
+
+    fetch("https://api.linkpreview.net", {
+      method: "POST",
+      mode: "cors",
+      body: JSON.stringify(data),
+    })
+      .then(res => res.json())
+      .then(res => {
+        console.log(res);
+        setResponse(res);
+      });
+  };
+
+  return (
+    <>
+      <h1>Gift Page</h1>
+      <form onSubmit={handleSubmit}>
+        {/* <label htmlFor="url-input">url을 입력하세요</label> */}
+        <input id="url-input" type="text" onChange={handleUrlChange} />
+        <button type="submit">제출하기</button>
+        {response && <img src={response.image || ""} alt="이미지" />}
+        {response && <h1>{response.title}</h1>}
+      </form>
+    </>
+  );
 }
