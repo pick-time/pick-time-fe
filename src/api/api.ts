fedd8d70a87d8b6200ae1fc00023a068fee1ddca
blcklamb <whenucan35@gmail.com>
2023-06-26 00:32:23 +0900

    feat: useGETGiftList - 주는 사람, 받는 사람이 선택한 선물 목록을 확인할 때 쓰는 api

--- src/api/api.ts
+++ src/api/api.ts
@@ -1,4 +1,7 @@
 import axios from "axios";
+import { useQuery } from "react-query";
+import { CouponList } from "types/couponList.type";
+import { GiftList } from "types/giftList.type";
 import type { Todos } from "types/todo.type";
 
 // API 호출 함수
@@ -7,6 +10,42 @@ async function fetchTodos(): Promise<Todos[]> {
     "https://jsonplaceholder.typicode.com/todos",
   );
   return response.data;
+}
+
+async function getGiftList({
+  targetId,
+}: GETGiftListRequest): Promise<GETGiftListResponse> {
+  return axios
+    .get<GETGiftListResponse>(`/gift/${targetId}`, {
+      params: targetId,
+    })
+    .then(response => response.data);
+}
+/**
+ *
+ *
+ * @param targetId
+ * @returns GETGiftListResponse
+ */
+export function useGETGiftList({ targetId }: { targetId: number }) {
+  return useQuery<GETGiftListResponse>({
+    queryKey: ["result", targetId],
+    queryFn: () => getGiftList({ targetId }),
+    refetchOnWindowFocus: false,
+    retry: 0,
+  });
+}
+
+interface GETGiftListRequest {
+  targetId: number;
+}
+
+export interface GETGiftListResponse {
+  giftTotal: number;
+  providerName: string;
+  consumerName: string;
+  giftList: GiftList[];
+  couponList: CouponList[];
 }
 
 export default fetchTodos;
