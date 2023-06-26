1d723fb471f9800a6648a9733fa5132c396b6ab9
blcklamb <whenucan35@gmail.com>
2023-06-26 17:34:41 +0900

    fix: 배포 url 설정

--- src/api/api.ts
+++ src/api/api.ts
@@ -15,18 +15,20 @@ export default async function fetchTodos(): Promise<Todos[]> {
 }
 
 export async function postScrapeMetaData(url: string) {
+  console.log(url);
   const res = await axios({
     // url: "http://localhost:5151/scrape",
-    url: "https://port-0-pick-time-express-7xwyjq992lljchlygq.sel4.cloudtype.app/scrape",
+    // url: "https://political-olive-radio.glitch.me/scrape",
+    url: "/scrape", // 이거다!
     method: "post",
     data: {
       url,
     },
   });
+
   console.log(res);
   if (res.status === 200) {
     console.log("test");
-
     return res.data;
   }
   throw new Error(res.statusText);
