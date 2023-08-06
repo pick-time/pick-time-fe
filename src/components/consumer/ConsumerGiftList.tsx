038ae66114abdb40c32f63f440e101ee9e85a8a8
blcklamb <whenucan35@gmail.com>
2023-08-06 18:06:10 +0900

    feat: 쿠폰 데이터 에러 처리 - 쿠폰 이미지 url이 유효하지 않은 경우 일괄 기본 쿠폰이미지 처리

--- src/components/consumer/ConsumerGiftList.tsx
+++ src/components/consumer/ConsumerGiftList.tsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import COLOR from "style/color";
 import styled from "styled-components";
 import { CouponList } from "types/couponList.type";
@@ -8,21 +9,71 @@ interface ConsumerGiftListProps {
   couponList?: CouponList[];
 }
 
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+const ErrorCoupon = () => {
+  return <CouponPreview>COUPON</CouponPreview>;
+};
+
+const CouponPreview = styled.div`
+  width: 26.1rem;
+  height: 12.4rem;
+  border-radius: 8px;
+  background: linear-gradient(133deg, #52ccff 0%, #5448e8 100%);
+
+  position: relative;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+
+  color: ${COLOR.WHITE};
+  text-align: center;
+  font-size: 2rem;
+  font-weight: 700;
+`;
+
 export default function ConsumerGiftList({
   giftList,
   couponList,
 }: ConsumerGiftListProps) {
+  const [selectedCouponId, setSelectedCouponId] = useState<number | null>(null);
+
   return (
-    <>
+    <form>
       {couponList && (
         <>
           <Divider />
           <ListTitleWrapper>
             <ListTitleText>쿠폰 리스트</ListTitleText>
           </ListTitleWrapper>
-          {couponList.map(ele => {
-            return ele.couponId;
-          })}
+          <ListWrapper>
+            {couponList.map(ele => {
+              const { couponId, couponTitle, couponImage } = ele;
+              const [couponIsError, setCouponIsError] =
+                useState<boolean>(false);
+              return (
+                <CouponListItem key={couponId}>
+                  <CouponListItemInfoWrapper>
+                    {couponImage && !couponIsError && (
+                      <CouponListImage
+                        alt={couponTitle}
+                        src={couponImage}
+                        onError={() => setCouponIsError(true)}
+                      />
+                    )}
+                    {(!couponImage || couponIsError) && <ErrorCoupon />}
+                  </CouponListItemInfoWrapper>
+                  <CouponListButtonWrapper>
+                    <RadioButton
+                      type="radio"
+                      checked={selectedCouponId === couponId}
+                      onChange={() => setSelectedCouponId(couponId)}
+                    />
+                  </CouponListButtonWrapper>
+                </CouponListItem>
+              );
+            })}
+          </ListWrapper>
         </>
       )}
       {giftList && (
@@ -31,12 +82,14 @@ export default function ConsumerGiftList({
           <ListTitleWrapper>
             <ListTitleText>상품 리스트</ListTitleText>
           </ListTitleWrapper>
-          {giftList.map(ele => {
-            return ele.giftId;
-          })}
+          <ListWrapper>
+            {giftList.map(ele => {
+              return ele.giftId;
+            })}
+          </ListWrapper>
         </>
       )}
-    </>
+    </form>
   );
 }
 
@@ -59,4 +112,67 @@ const ListTitleText = styled.span`
   font-size: 1.4rem;
   font-weight: 700;
   margin: 0.8rem 0;
+`;
+
+const ListWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  gap: 1.6rem;
+  margin-bottom: 1.6rem;
+`;
+
+const CouponListItem = styled.div`
+  height: 12.4rem;
+  width: 30.8rem;
+  display: flex;
+  background-color: ${COLOR.PLACEHOLDER_PURPLE};
+  border-radius: 1rem;
+
+  margin-bottom: 1.6rem;
+  justify-content: space-between;
+  margin: auto;
+`;
+
+const CouponListItemInfoWrapper = styled.div`
+  display: flex;
+  width: 30.8rem;
+`;
+
+const CouponListImage = styled.img`
+  width: auto;
+  height: 100%;
+  border-radius: 1rem;
+  margin-right: 0.8rem;
+`;
+
+const CouponListButtonWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  max-width: 2rem;
+  margin: 0.7rem 1rem 0.7rem 0rem;
+`;
+
+const RadioButton = styled.input`
+  appearance: none;
+  border: 0.1em solid ${COLOR.PURPLE};
+  border-radius: 50%;
+  background: ${COLOR.PLACEHOLDER_PURPLE};
+  width: 1.25em;
+  height: 1.25em;
+
+  &:before {
+    content: "";
+    transform: scale(0);
+    transition: 120ms transform ease-in-out;
+    box-shadow: inset 1rem 1rem ${COLOR.PURPLE};
+  }
+
+  &:checked {
+    &::before {
+      transform: scale(1);
+    }
+  }
 `;
