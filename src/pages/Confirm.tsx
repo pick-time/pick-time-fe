cb3638b26404113c3f691c862b07fc854bc79b70
blcklamb <whenucan35@gmail.com>
2023-10-24 21:24:32 +0900

    feat: 완료 플로우 연결 및 확인 페이지 완성 - provider가 데이터 작성 후 완료하는 플로우 연결 - 데이터 확인하는 페이지에서 쿠폰 render 추가

--- src/pages/Confirm.tsx
+++ src/pages/Confirm.tsx
@@ -1,7 +1,6 @@
 /* eslint-disable no-nested-ternary */
-/* eslint-disable @typescript-eslint/no-unused-vars */
+
 import { useGetResult } from "hooks/queries/useResult";
-import { Result } from "types/result.type";
 import Button from "components/common/Button";
 import Header from "components/common/Header";
 import ListComponent from "components/common/List";
@@ -10,8 +9,6 @@ import Title from "components/common/Title";
 import { useParams, useNavigate } from "react-router-dom";
 import COLOR from "style/color";
 import styled from "styled-components";
-
-type User = Pick<Result, "providerName" | "consumerName">;
 
 function Confirm() {
   const { targetId } = useParams();
@@ -42,6 +39,7 @@ function Confirm() {
             </Title>
           </TitleWrapper>
           <ListComponent giftList={data?.giftList} type="default" />
+          <ListComponent couponList={data?.couponList} type="default" />
         </>
       )}
       <ButtonWrapper>
